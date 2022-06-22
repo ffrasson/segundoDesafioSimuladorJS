debd76e03af7159f2a559dc60862a5c8d4a43776
ffrasson <fernando.frasson@gmail.com>
2022-06-22 01:21:03 -0300

    Segunda Version

--- js/index.js
+++ js/index.js
@@ -1,59 +1,60 @@
 /* 
-* Este algoritmo se encarga de mostrar el menu y de solicitar el número de cabañas que desea arrendar.
+* El presente algoritmo se utilizará para mostrar el menu y solicitar la cantidad de campers que desea alquilar.
 * Si el usuario ingresa un número que no está entre 1 y 2, se le mostrará un mensaje de error.
 * Si el usuario ingresa un número que es igual a ESC, se le mostrará un mensaje de salida.
-* Luego pedirá la cantidad de adultos con un máximo de la cantidad de personas que puede arrendar.
-* Luego pedirá la cantidad de niños con un máximo de la cantidad de personas que puede arrendar.
-* Si el usuario ingresa un número que no está entre 0 y la cantidad de personas que puede arrendar, se le mostrará un mensaje de error.
+* Luego pedirá la cantidad de adultos con un máximo de la cantidad de personas que puede alquilar.
+* Es condicion que por lo menos haya un adulto para alquilar.
+* Luego pedirá la cantidad de niños con un máximo de la cantidad de personas que puede alquilar.
+* Si el usuario ingresa un número que no está entre 0 y la cantidad de personas que puede alquilar, se le mostrará un mensaje de error.
 */
 
-function oneCabin(adults, kids){
-  let total = adults + kids;
-      if (adults + kids >= 7 ){
+function unaCamper(adultos, niños){
+  let total = adultos + niños;
+      if (adultos + niños >= 7 ){
                   /* Una alerta que se activa cuando el número de personas supera las 6. */
-                  alert('Sobrepasa el limite de personas por cabaña, contrate dos si desea mas capacidad');
+                  alert('Sobrepasa el limite de personas por camper, contrate dos si desea mas capacidad');
               }	
   return total;
 }
 
-function twoCabin(adults, kids){
-  let total = adults + kids;
-  if (adults + kids >= 13 ){
+function dosCamper(adultos, niños){
+  let total = adultos + niños;
+  if (adultos + niños >= 13 ){
                   /* Una alerta que se activa cuando el número de personas supera las 12. */
-                  alert('Sobrepasa el limite de personas máximo por cabaña, si son mas personas favor contactar a nuestro personal');
+                  alert('Sobrepasa el limite de personas máximo por camper, si son mas personas favor contactar a nuestro personal');
               }	
   return total;
 }
 
-function showPrice(total){ //muestra el total de personas
+function muestraCantidad(total){ //muestra el total de personas
   alert('El total de personas ingresadas es : '+ total);
 }
 
-function showMenu(menu){ // muestra el menú
-  let options = prompt('Número de cabañas que desea arrendar. (ESC) para salir \n 1. Una cabaña hasta 6 personas \n 2. Dos cabañas hasta 12 personas \n');
-  return options;
+function muestraMenu(menu){ // muestra el menú
+  let opciones = prompt('Número de campers que desea alquilar. (ESC) para salir \n 1. Una camper hasta 6 personas \n 2. Dos campers hasta 12 personas \n');
+  return opciones;
 }
 
-function quotation(){ //cotizamos las cabañas
-  let selectedOption = showMenu(); 
-  while(selectedOption !== 'ESC'){
-      if(!isNaN(selectedOption !== '')){
-          let adults = parseInt(prompt('Ingrese el número de adultos'));
-          let kids = parseInt(prompt('Ingrese el número de niños'));
-              if (adults <= 0 && kids !== 0){
-                  alert('No se puede arrendar una cabañas sin adultos');
+function alquiler(){ //cotizamos el alquiler de las campers en funcion de la cantidad de personas ingresadas
+  let opcionSeleccionada = muestraMenu(); 
+  while(opcionSeleccionada !== 'ESC'){
+      if(!isNaN(opcionSeleccionada !== '')){
+          let adultos = parseInt(prompt('Ingrese la cantidad de adultos'));
+          let niños = parseInt(prompt('Ingrese la cantidad de niños'));
+              if (adultos <= 0 && niños !== 0){
+                  alert('No se puede alquilar una camper sin adultos');
               }
-              selectedOption = parseInt(selectedOption);
+              opcionSeleccionada = parseInt(opcionSeleccionada);
                //Switch seleccionar la opción
               
-              switch(selectedOption){
-                  case 1: // una cabaña
-                      let totalOne = oneCabin(adults, kids);
-                      showPrice(totalOne);
+              switch(opcionSeleccionada){
+                  case 1: // una camper
+                      let totalUno = unaCamper(adultos, niños);
+                      muestraCantidad(totalUno);
                       break;
-                  case 2: // dos cabañas
-                      let totalTwo = twoCabin(adults, kids);
-                      showPrice(totalTwo);
+                  case 2: // dos camper
+                      let totalDos = dosCamper(adultos, niños);
+                      muestraCantidad(totalDos);
                       break;
                   default:
                       alert('Opción no válida');
@@ -62,8 +63,8 @@ function quotation(){ //cotizamos las cabañas
       }else{
           alert('Seleccione una opción válida');
       }
-      selectedOption = showMenu();   
+      opcionSeleccionada = muestraMenu();   
   }
 }
 /* Llamando a la función principal. */
-quotation();
+alquiler();
